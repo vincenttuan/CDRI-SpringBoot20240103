a52297c4e22c68abd68631237643e4b39097f776
aweit <aa4192696@gmail.com.tw>
2024-01-03 12:29:54 +0800

    java

---
+++ SpringBootStudentScore/src/main/java/com/example/demo/controller/StudentScoreController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.entity.StudentScore;
+import com.example.demo.repository.StudentScoreRepository;
+
+@Controller
+@RequestMapping("/student-score")
+public class StudentScoreController {
+	
+	@Autowired
+	private StudentScoreRepository studentScoreRepository;
+	
+	@GetMapping("/")
+	@ResponseBody
+	public String index() {
+		return studentScoreRepository.findAll().toString();
+	}
+	
+	@PostMapping
+	@ResponseBody
+	public String add(StudentScore studentScore) {
+		studentScoreRepository.save(studentScore);
+		return "Add OK";
+	}
+	
+}
