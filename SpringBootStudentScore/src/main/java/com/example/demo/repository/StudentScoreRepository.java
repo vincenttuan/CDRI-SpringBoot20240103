e6ed510b7f2a3863740c84420b572fd4385878a2
aweit <aa4192696@gmail.com.tw>
2024-01-03 16:26:38 +0800

    java

--- SpringBootStudentScore/src/main/java/com/example/demo/repository/StudentScoreRepository.java
+++ SpringBootStudentScore/src/main/java/com/example/demo/repository/StudentScoreRepository.java
@@ -1,9 +1,26 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.entity.StudentScore;
 
+import jakarta.transaction.Transactional;
+
 public interface StudentScoreRepository extends JpaRepository<StudentScore, Integer> {
+	
+	// 只要修改學生名字的方法
+	@Modifying
+	@Transactional
+	@Query("UPDATE StudentScore s SET s.name = :name WHERE s.id = :id ") // JPQL
+	void updateNameById(@Param("id") Integer id, @Param("name") String name);
+	
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE student_score SET name = :name WHERE id = :id ", nativeQuery = true) // SQL
+	void updateNameByIdNativeSQL(@Param("id") Integer id, @Param("name") String name);
+	
 	
 }
