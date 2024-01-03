6f7c22754e10c810ebedf14d565a4fa1625b950b
aweit <aa4192696@gmail.com.tw>
2024-01-03 12:16:26 +0800

    java

---
+++ SpringBootStudentScore/src/main/java/com/example/demo/entity/StudentScore.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity // 預設 @Entity(name = "student_score")
+public class StudentScore {
+	@Id // 主鍵
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	// @Column(name = "name", length = 255, nullable = true, unique = false) 預設值可以不寫
+	private String name;
+	private Integer chineseScore;
+	private Integer englishScore;
+	private Integer mathScore;
+	private Integer totalScore;
+	private Double averageScore;
+	
+}
