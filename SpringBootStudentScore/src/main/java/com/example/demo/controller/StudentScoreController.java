65367e8ae61a9d6a9644c5a07a0ca614f963d84d
aweit <aa4192696@gmail.com.tw>
2024-01-03 14:03:33 +0800

    java

--- SpringBootStudentScore/src/main/java/com/example/demo/controller/StudentScoreController.java
+++ SpringBootStudentScore/src/main/java/com/example/demo/controller/StudentScoreController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.entity.StudentScore;
 import com.example.demo.repository.StudentScoreRepository;
+import com.github.javafaker.Faker;
 
 @Controller
 @RequestMapping("/student-score")
@@ -36,6 +38,24 @@ public class StudentScoreController {
 		return "";
 	}
 	
+	// 產生 100 筆測試資料
+	@GetMapping("/add/100")
+	@ResponseBody
+	public String add100() {
+		Faker faker = new Faker();
+		Random random = new Random();
+		for(int i=0;i<100;i++) {
+			StudentScore studentScore = new StudentScore();
+			studentScore.setName(faker.name().fullName());
+			studentScore.setChineseScore(random.nextInt(101));
+			studentScore.setEnglishScore(random.nextInt(101));
+			studentScore.setMathScore(random.nextInt(101));
+			studentScore.updateTotalAndAverage();
+			studentScoreRepository.save(studentScore);
+		}
+		return "add 100 OK";
+	}
+	
 	@PostMapping("/")
 	@ResponseBody
 	public String add(StudentScore studentScore) {
@@ -43,5 +63,7 @@ public class StudentScoreController {
 		studentScoreRepository.save(studentScore);
 		return "Add OK: " + studentScore;
 	}
+	
+	
 	
 }
