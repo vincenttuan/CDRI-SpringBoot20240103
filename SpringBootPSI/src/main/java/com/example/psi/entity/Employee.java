f767a1380c7872985568e572aca45d61846f0e9d
aweit <aa4192696@gmail.com.tw>
2024-01-04 14:14:11 +0800

    java

--- SpringBootPSI/src/main/java/com/example/psi/entity/Employee.java
+++ SpringBootPSI/src/main/java/com/example/psi/entity/Employee.java
@@ -1,5 +1,19 @@
 package com.example.psi.entity;
 
-public class Employee {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "employee")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 員工序號
+	
+	@Column
+	private String name; // 員工姓名
 }
