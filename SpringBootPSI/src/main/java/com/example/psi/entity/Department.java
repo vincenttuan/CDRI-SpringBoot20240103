18e96ef4127d46bee556097189c1171402d76b74
aweit <aa4192696@gmail.com.tw>
2024-01-04 14:58:50 +0800

    java

--- SpringBootPSI/src/main/java/com/example/psi/entity/Department.java
+++ SpringBootPSI/src/main/java/com/example/psi/entity/Department.java
@@ -1,10 +1,15 @@
 package com.example.psi.entity;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -19,5 +24,8 @@ public class Department {
 	@Column
 	private String name; // 部門名稱
 	
+	@OneToMany(mappedBy = "department")
+	@OrderBy("id ASC")
+	private Set<Employee> emplopyees = new LinkedHashSet<>();
 	
 }
