52983ec5342d3e4f2be2993c31bb962a3b94f8ed
aweit <aa4192696@gmail.com.tw>
2024-01-04 14:26:51 +0800

    java

--- SpringBootPSI/src/main/java/com/example/psi/entity/PurchaseItem.java
+++ SpringBootPSI/src/main/java/com/example/psi/entity/PurchaseItem.java
@@ -1,5 +1,22 @@
 package com.example.psi.entity;
 
-public class PurchaseItem {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Table(name = "purchase_item")
+@Data
+public class PurchaseItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 採購項目序號
+	
+	@Column
+	private Integer amount; // 採購數量
 }
