395bc38e4ced2873abd2dd9b978750054451aff4
aweit <aa4192696@gmail.com.tw>
2024-01-04 14:22:19 +0800

    java

--- SpringBootPSI/src/main/java/com/example/psi/entity/Product.java
+++ SpringBootPSI/src/main/java/com/example/psi/entity/Product.java
@@ -1,5 +1,26 @@
 package com.example.psi.entity;
 
-public class Product {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "product")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 商品序號
+	
+	@Column
+	private String name; // 商品名稱
+	
+	@Column
+	private Integer cost; // 商品成本
+	
+	@Column
+	private Integer price; // 商品定價
+	
 }
