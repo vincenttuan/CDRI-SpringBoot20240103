1d55d6004b1d4a59d6f4372c32ecf3ad6cb0d1d9
aweit <aa4192696@gmail.com.tw>
2024-01-04 14:16:15 +0800

    java

--- SpringBootPSI/src/main/java/com/example/psi/entity/Order.java
+++ SpringBootPSI/src/main/java/com/example/psi/entity/Order.java
@@ -1,5 +1,27 @@
 package com.example.psi.entity;
 
-public class Order {
+import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "orders") // 因為 MySQL 不可以建立 Order 資料表 (因 Order 在 MySQL 中是保留字)
+public class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 員工序號
+	
+	@Column
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date date; // 訂單日期
 }
