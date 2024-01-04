80cda13b128fb67e9fc82dc5c9df542f156e44f7
aweit <aa4192696@gmail.com.tw>
2024-01-04 14:28:47 +0800

    java

--- SpringBootPSI/src/main/java/com/example/psi/entity/Customer.java
+++ SpringBootPSI/src/main/java/com/example/psi/entity/Customer.java
@@ -1,6 +1,5 @@
 package com.example.psi.entity;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
