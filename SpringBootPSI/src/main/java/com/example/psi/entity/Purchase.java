a0b28fc32fc5747b9ac6aeebb8b7bcf6e7ac6e75
aweit <aa4192696@gmail.com.tw>
2024-01-04 14:23:40 +0800

    java

--- SpringBootPSI/src/main/java/com/example/psi/entity/Purchase.java
+++ SpringBootPSI/src/main/java/com/example/psi/entity/Purchase.java
@@ -1,5 +1,28 @@
 package com.example.psi.entity;
 
-public class Purchase {
+import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "purchase")
+public class Purchase {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 採購單序號
+	
+	@Column
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date date; // 採購日期
+	
 }
