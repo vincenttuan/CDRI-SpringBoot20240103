fc6a6177fe1056776533c80eab857f156582f36c
aweit <aa4192696@gmail.com.tw>
2024-01-08 10:45:30 +0800

    java

---
+++ SpringBootPSI/src/test/java/com/example/psi/testing/create/CreateEmployee.java
@@ -0,0 +1,57 @@
+package com.example.psi.testing.create;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.psi.model.dto.DepartmentDTO;
+import com.example.psi.model.dto.EmployeeDTO;
+import com.example.psi.model.po.Department;
+import com.example.psi.service.DepartmentService;
+import com.example.psi.service.EmployeeService;
+
+@SpringBootTest
+public class CreateEmployee {
+	@Autowired
+	private EmployeeService employeeService;
+	
+	// 模擬 controller 新增員工
+	@Test
+	public void addTest() {
+		
+		EmployeeDTO emp1 = new EmployeeDTO();
+		emp1.setName("劉一");
+		
+		EmployeeDTO emp2 = new EmployeeDTO();
+		emp2.setName("陳二");
+		
+		EmployeeDTO emp3 = new EmployeeDTO();
+		emp3.setName("張三");
+		
+		EmployeeDTO emp4 = new EmployeeDTO();
+		emp4.setName("李四");
+		
+		EmployeeDTO emp5 = new EmployeeDTO();
+		emp5.setName("王五");
+		
+		EmployeeDTO emp6 = new EmployeeDTO();
+		emp6.setName("趙六");
+		
+		EmployeeDTO emp7 = new EmployeeDTO();
+		emp7.setName("孫七");
+		
+		EmployeeDTO emp8 = new EmployeeDTO();
+		emp8.setName("周八");
+		
+		EmployeeDTO emp9 = new EmployeeDTO();
+		emp9.setName("吳九");
+		
+		EmployeeDTO emp10 = new EmployeeDTO();
+		emp10.setName("鄭十");
+		
+		// 儲存
+		
+		System.out.println("Save OK !");
+	}
+	
+}
