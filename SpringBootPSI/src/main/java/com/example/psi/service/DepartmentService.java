925d50230823835e53715ed4a672824454e9529c
aweit <aa4192696@gmail.com.tw>
2024-01-08 10:22:54 +0800

    java

--- SpringBootPSI/src/main/java/com/example/psi/service/DepartmentService.java
+++ SpringBootPSI/src/main/java/com/example/psi/service/DepartmentService.java
@@ -3,6 +3,7 @@ package com.example.psi.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.psi.model.dto.DepartmentDTO;
 import com.example.psi.model.po.Department;
 import com.example.psi.repository.DepartmentRepository;
 
@@ -13,7 +14,10 @@ public class DepartmentService {
 	private DepartmentRepository departmentRepository;
 	
 	public void add(DepartmentDTO departmentDTO) {
-		
+		// 將 departmentDTO 轉 department
+		Department department = new Department();
+		department.setName(department.getName());
+		departmentRepository.save(department);
 	}
 	
 }
