9ba92b39194a67e6d8d9a217e14d8c912f119078
aweit <aa4192696@gmail.com.tw>
2024-01-08 11:21:59 +0800

    java

---
+++ SpringBootPSI/src/test/java/com/example/psi/testing/read/ReadDepartment.java
@@ -0,0 +1,21 @@
+package com.example.psi.testing.read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.psi.model.dto.DepartmentDTO;
+import com.example.psi.service.DepartmentService;
+
+@SpringBootTest
+public class ReadDepartment {
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	@Test
+	public void readOne() {
+		DepartmentDTO departmentDTO = departmentService.getDepartmentById(1L);
+		System.out.println(departmentDTO);
+	}
+}
