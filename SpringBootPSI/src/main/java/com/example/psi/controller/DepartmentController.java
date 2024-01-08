b1b7bd6fdc5a0bd3285c0653e699f1e9ac8dc9d8
aweit <aa4192696@gmail.com.tw>
2024-01-08 12:21:13 +0800

    java

---
+++ SpringBootPSI/src/main/java/com/example/psi/controller/DepartmentController.java
@@ -0,0 +1,29 @@
+package com.example.psi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.psi.model.dto.DepartmentDTO;
+import com.example.psi.service.DepartmentService;
+
+@Controller
+@RequestMapping("/department")
+public class DepartmentController {
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	@GetMapping("/")
+	public String index(@ModelAttribute DepartmentDTO departmentDTO, Model model) {
+		List<DepartmentDTO> departments = departmentService.findAll();
+		model.addAttribute("departments", departments);
+		return "department";
+	}
+	
+}
