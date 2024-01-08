522dea38295e962bc592dc24828d4d946b274bd4
aweit <aa4192696@gmail.com.tw>
2024-01-08 15:21:18 +0800

    java

--- SpringBootPSI/src/main/java/com/example/psi/controller/EmployeeController.java
+++ SpringBootPSI/src/main/java/com/example/psi/controller/EmployeeController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -42,5 +45,32 @@ public class EmployeeController {
 		model.addAttribute("departments", departments);
 		model.addAttribute("employeePageDTO", employeePageDTO);
 		return "employee";
+	}
+	
+	@PostMapping("/")
+	public String create(EmployeeDTO employeeDTO) {
+		employeeRepository.save(employee);
+		return "redirect:/employee/";
+	}
+	
+	@GetMapping("/edit/{id}") // 修改頁面的呈現
+	public String edit(@PathVariable("id") Long id, Model model) {
+		Employee employee = employeeRepository.findById(id).get();
+		model.addAttribute("employee", employee);
+		model.addAttribute("departments", departmentRepository.findAll());
+		return "employee-edit";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable("id") Long id, Employee employee) {
+		employee.setId(id);
+		employeeRepository.save(employee);
+		return "redirect:/employee/";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		employeeRepository.deleteById(id);
+		return "redirect:/employee/";
 	}
 }
--- SpringBootPSI/src/main/java/com/example/psi/service/EmployeeService.java
+++ SpringBootPSI/src/main/java/com/example/psi/service/EmployeeService.java
@@ -1,11 +1,14 @@
 package com.example.psi.service;
 
+import java.util.Optional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.example.psi.model.dto.DepartmentDTO;
 import com.example.psi.model.dto.EmployeeDTO;
 import com.example.psi.model.dto.EmployeePageDTO;
 import com.example.psi.model.po.Department;
@@ -24,12 +27,17 @@ public class EmployeeService {
 	@Autowired
 	private ModelMapper modelMapper;
 	
+	// 新增 I
 	public void add(EmployeeDTO employeeDTO) {
 		// employeeDTO 轉 employee
 		Employee employee = modelMapper.map(employeeDTO, Employee.class);
+		// 根據指定 employeeDTO 的 department's Id 注入 department 物件
+		Department department = departmentRepository.findById(employeeDTO.getDepartment().getId()).get();
+		employee.setDepartment(department);
 		employeeRepository.save(employee);
 	}
 	
+	// 新增 II
 	public void add(EmployeeDTO employeeDTO, Long departmentId) {
 		// employeeDTO 轉 employee
 		Employee employee = modelMapper.map(employeeDTO, Employee.class);
@@ -37,6 +45,38 @@ public class EmployeeService {
 		Department department = departmentRepository.findById(departmentId).get();
 		employee.setDepartment(department);
 		employeeRepository.save(employee);
+	}
+	
+	// 修改
+	public void update(EmployeeDTO employeeDTO, Long id) {
+		Optional<Employee> employeeOpt = employeeRepository.findById(id);
+		if(employeeOpt.isPresent()) {
+			Employee employee = employeeOpt.get();
+			employee.setName(employeeDTO.getName()); // 更新員工姓名
+			Department department = departmentRepository.findById(employeeDTO.getDepartment().getId()).get(); // 找到員工部門
+			employee.setDepartment(department); // 更新員工部門
+			employeeRepository.save(employee);
+		} 
+	}
+	
+	// 刪除
+	public void delete(Long id) {
+		Optional<Employee> employeeOpt = employeeRepository.findById(id);
+		if(employeeOpt.isPresent()) {
+			employeeRepository.delete(employeeOpt.get());
+		} 
+	}
+	
+	// 查詢單筆
+	public DepartmentDTO getDepartmentById(Long id) {
+		Optional<Department> departmentOpt = departmentRepository.findById(id);
+		if(departmentOpt.isPresent()) {
+			Department department = departmentOpt.get();
+			// department 轉 departmentDTO
+			DepartmentDTO departmentDTO = modelMapper.map(department, DepartmentDTO.class);
+			return departmentDTO;
+		}
+		return null;
 	}
 	
 	// 全部查詢(分頁)
