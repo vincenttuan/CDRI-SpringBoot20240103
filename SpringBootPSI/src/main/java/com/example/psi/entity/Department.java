5cee03077d6b11362103b1aa8606075a23e47d41
aweit <aa4192696@gmail.com.tw>
2024-01-08 09:07:07 +0800

    java

--- SpringBootPSI/src/main/java/com/example/psi/entity/Department.java
+++ SpringBootPSI/src/main/java/com/example/psi/entity/Department.java
@@ -13,17 +13,30 @@ import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-@Entity
+@Entity // PO (Persistent Object): 持久對象是一種在資料庫中有對應記錄的對象。
 @Table(name = "department")
 @Data
 public class Department {
+	// @Id: 主鍵, POID (Persistent Object Identifier): 持久對象標識符是一個唯一標識 PO 的值，通常對應於資料庫表中的主鍵。
+	// @GeneratedValue: 自動產生值
+	// strategy = GenerationType.IDENTITY : 自動增長, 適用於 MySQL
+	// strategy = GenerationType.SEQUENCE : 自動增長, 適用於 Oracle
+	// strategy = GenerationType.TABLE : 自動增長, 適用於所有資料庫
+	// GenerationType.TABLE 會建立一個資料表, 用來紀錄目前的 id 值, 並且鎖定該筆資料, 避免同時新增時發生重複
+	// strategy = GenerationType.AUTO : 自動增長, 由系統自動選擇適合的策略
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id; // 部門序號
 	
-	@Column
+	//@Column(name = "name", unique = false, nullable = true, length = 255) // 預設
+	@Column(name = "name", unique = true, nullable = false, length = 50) // 自訂
 	private String name; // 部門名稱
 	
+	// mappedBy = "department" 表示此集合的內容是由 Employee 的 department 來維護
+	// fetch = FetchType.LAZY (預設)表示預設不會主動查詢該部門的員工(有用到時才會去查詢)
+	// fetch = FetchType.EAGER 表示預設會主動查詢該部門的員工(不論是否會用都會先查好)
+	// orphanRemoval = true 表示如果該部門下所有 Employee 被刪除, 則此部門也會被刪除(在此不適用)
+	// orphanRemoval = false (預設)表示如果該部門下所有 Employee 被刪除, 則此部門並不會被刪除
 	@OneToMany(mappedBy = "department")
 	@OrderBy("id ASC")
 	private Set<Employee> emplopyees = new LinkedHashSet<>();
