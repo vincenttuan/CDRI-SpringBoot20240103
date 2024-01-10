c3a9b3f4c70d8dfc461a9276244f92fbcb9e46f6
aweit <aa4192696@gmail.com.tw>
2024-01-10 16:15:25 +0800

    java

---
+++ SpringBootInvest/src/main/java/com/example/invest/SpringBootInvestApplication.java
@@ -0,0 +1,13 @@
+package com.example.invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInvestApplication.class, args);
+	}
+
+}
