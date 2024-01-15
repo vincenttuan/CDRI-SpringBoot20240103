24fc22adea4dfb39d0a050ac43d79b50ed6c8ce7
aweit <aa4192696@gmail.com.tw>
2024-01-15 13:47:51 +0800

    java

--- SpringBootPortfolio/src/main/java/com/example/portfolio/ServletInitializer.java
+++ SpringBootPortfolio/src/main/java/com/example/portfolio/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootInvestApplication.class);
+		return application.sources(SpringBootPortfolioApplication.class);
 	}
 
 }
